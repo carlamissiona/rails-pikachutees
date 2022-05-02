170fe085468f446cf78307ff458a43b0fe8b04a6
carlamissiona <missiona.carla@gmail.com>
2022-05-02 23:33:25 +0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   def index
-    
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
 
     @page_name = "home"
    
