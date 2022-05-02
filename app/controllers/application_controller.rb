6eb1a346dae26f184a76c804abed0c2b88f73fb4
carlamissiona <missiona.carla@gmail.com>
2022-05-02 23:09:18 +0800

    up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  def index
+    
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+
+    @page_name = "home"
+   
+    
+  end
+
+
 end
