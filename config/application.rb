d8fa296d273bab2f94a88b96b0d0e51af5883fc7
carlamissiona <missiona.carla@gmail.com>
2022-05-02 22:10:26 +0800

    up

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module RailsStarter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.serve_static_assets = true
+    config.public_file_server.enabled = true
   end
 end
